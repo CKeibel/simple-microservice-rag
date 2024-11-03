d421a0fe171c89ab2301d9b88b6ddb1a51b0f374
Christopher Keibel <christopher.keibel.90@gmail.com>
2024-11-03 16:52:17 +0100

    make endpoint async

--- reranker/main.py
+++ reranker/main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 
 
 @app.post("/rank", response_model=RankResponse)
-def rank(request: RankRequest, service=Depends(lambda: RankService())):
+async def rank(request: RankRequest, service=Depends(lambda: RankService())):
     results = service.rank(request.query, request.docs)
     return RankResponse(results=results)
